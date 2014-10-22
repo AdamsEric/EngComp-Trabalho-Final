c910c54264e6b0e001bd0f02fe284328442ed8ec
Eric Adams <eric.adsbrito@yahoo.com.br>
2014-10-22 07:16:47 -0300

    Update Trabalho.cpp
    Atualizado arquivo para versão 2.1

--- Trabalho.cpp
+++ Trabalho.cpp
@@ -3,36 +3,60 @@
 #include<string>
 using namespace std;
 
-struct TipoDisciplina
+class Disciplina
 {
+	private:
 		string nome;
-		int ch, tipoaula;
+		int ch;
+		int tipoaula;
+	public:
+		void set_nome(string nome);
+		void set_ch (int ch);
+		void set_tipoaula(int tipoaula);
+		
+		string get_nome(){
+			return nome;
+		};
+		int get_ch(){
+			return ch;
+		};
+		int get_tipoaula(){
+			return tipoaula;
+		};
 };
 
-TipoDisciplina disciplina[2];
-	
-struct TipoProfessor
+/*class Professor
 {
+	public:
 		string nome;
 		enum hordisponivel{manha, tarde, noite};
-};
+};*/
 
 int CadastroDisciplinas(int x)
 {
+	string nome;
+	int ch;
+	int tipoaula;
+	
+	Disciplina disciplina[2];
 	cout << "Digite o nome da disciplina: ";
-	getline(cin, disciplina[x].nome);	
+	getline(cin, nome);
+	disciplina[x].set_nome(nome);
 	cout << "Digite a carga horaria da disciplina: ";
-	cin >> disciplina[x].ch;
-	if (disciplina[x].ch == 80){ 
+	cin >> ch;
+	disciplina[x].set_ch(ch);
+	if (ch == 80){ 
 		cout << "Digite 1 para disciplina teorica ou 2 para essencialmente pratica: ";
-		cin >> disciplina[x].tipoaula;	
+		cin >> tipoaula;
+		disciplina[x].set_tipoaula(tipoaula);
 	}
 	return 0;
 }
 
 int main()
 {
-	TipoProfessor professor[2];
+	Disciplina disciplina[2];
+//	TipoProfessor professor[2];
 	int i;
 	
 	for (i=0; i<2; i++){
@@ -41,7 +65,7 @@ int main()
 	
 	cout << "\n\nLista de Disciplinas Cadastradas: \n\n";
 	for (i=0; i<2; i++){
-		cout << i+1 << " - " << disciplina[i].nome << " - " << "Carga horaria: " << disciplina[i].ch << ".\n";
+		cout << i+1 << " - " << disciplina[i].get_nome() << " - " << "Carga horaria: " << disciplina[i].get_ch() << ".\n";
 	}	
 	
 	return 0;
